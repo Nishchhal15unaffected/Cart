147169a0150813328c3801858bddb0e06eb10e51
Nishchhal Prajapat <47791668+Nishchhal15unaffected@users.noreply.github.com>
2021-03-04 17:36:14 +0530

    Update index.js

--- web devlopment/Reactjs/cart/src/index.js
+++ web devlopment/Reactjs/cart/src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import firebase from 'firebase';
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyCfWeS2611KRAyEvS9MVik9n-F6SgiYKvg",
-    authDomain: "cart-50731.firebaseapp.com",
-    projectId: "cart-50731",
-    storageBucket: "cart-50731.appspot.com",
-    messagingSenderId: "105713139241",
-    appId: "1:105713139241:web:d5c62d46e9fa3a5e036638"
+    apiKey: "demo",
+    authDomain: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
